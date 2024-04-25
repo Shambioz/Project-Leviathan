a81817c32142f87aac0aa2d961515c600ac723bd
KawaiiPanzerGirl777 <opprzemek1979@wp.pl>
2024-04-25 18:32:43 +0200

    PEW PEW WORKS

--- Assets/Scripts/scr_pew_pew_pew.cs
+++ Assets/Scripts/scr_pew_pew_pew.cs
@@ -4,39 +4,41 @@ using UnityEngine;
 
 public class scr_pew_pew_pew : MonoBehaviour
 {
+    public LineRenderer lr;
+    private GameObject drone;
+    private GameObject playerrro;
+    private Vector3 drone_position;
+    private Vector3 hit_marker;
 
-    public int layer_mask;
-
-    public LineRenderer line_renderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        line_renderer = this.GetComponent<LineRenderer>();
-        line_renderer.enabled = false;
+
+    }
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        drone = GameObject.Find("Cylinder.001");
+        playerrro = GameObject.Find("Main Camera");
+        lr.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-
-            line_renderer.enabled = true;
-
-            Ray ray = new Ray(transform.position, transform.forward);
-            RaycastHit hit;
-            layer_mask = ~LayerMask.GetMask("Ignore Raycast");
-
-
-            if (Physics.Raycast(ray, out hit, 10))
-            {
-                line_renderer.SetPositions(new Vector3[] { ray.origin, hit.point });
-            }
+            lr.enabled = true;
+            drone_position = drone.transform.position;
+            hit_marker = playerrro.transform.position + playerrro.transform.forward * 2;
+            lr.SetPosition(0, drone_position);
+            lr.SetPosition(1, hit_marker);
         }
         else
         {
-            line_renderer.enabled = false;
+            lr.enabled = false;
         }
     }
 }
