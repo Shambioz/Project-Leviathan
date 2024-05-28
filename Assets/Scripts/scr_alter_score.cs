efbe916578eb32532b21e390b33c3a048de0f85a
Shambioz <theoshogo@gmail.com>
2024-05-28 16:23:58 +0200

    Artifact detection points foundation

---
+++ Assets/Scripts/scr_alter_score.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class scr_alter_score : MonoBehaviour
+{
+
+    public Collider thiefcollider;
+    public Collider eventbox;
+    public Collider compareEventbox;
+    public scr_score_control scr_score_control;
+    private bool buton = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventbox = gameObject.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (gameObject.GetComponent<Collider>().bounds.Intersects(thiefcollider.bounds))
+        {
+            scr_score_control.mortar = 500;
+        }
+
+        if (!gameObject.GetComponent<Collider>().bounds.Intersects(thiefcollider.bounds))
+        {
+            scr_score_control.mortar = 0;
+        }
+    }
+    */
+    private void OnTriggerStay(Collider other)
+    {
+        compareEventbox = other.GetComponent<Collider>();
+        if (compareEventbox = thiefcollider)
+        { 
+            scr_score_control.mortar = 500;
+            buton = true;
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (buton == true)
+        {
+            scr_score_control.mortar = 0;
+            buton = false;
+        }
+    }
+}
